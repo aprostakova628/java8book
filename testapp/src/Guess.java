75dd88ebd13c0efb2681209feb4ce4845180abb3
Alyona <aprostakova628@gmail.com>
2018-06-04 21:23:46 +0300

    The game of guessing letters, the second version

--- testapp/src/Guess.java
+++ testapp/src/Guess.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+class Guess {
+public static void main (String args[])
+    throws java.io.IOException {
+    char ch, answer = 'S';
 
-public class Guess {
+    System.out.println("Задумана буква из диапазона A-Z.");
+    System.out.print("Попытайтесь её угадать: ");
+    ch = (char) System.in.read();
+    if(ch == answer) System.out.println("** Правильно!**");
+    else {
+        System.out.print("...Извините, нужная буква находится ");
+        if (ch < answer)
+            System.out.println("ближе к концу алфавита");
+        else  System.out.println("ближе к началу алфавита");
+    }
+}
 }
